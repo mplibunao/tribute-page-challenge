b6a0b6cf8851d95713bcc732d158f1d950aeafa6
Mark Paolo Libunao <Mark Paolo Libunao>
2017-04-04 20:38:10 +0800

    oops. Was in styles directory

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # tribute-page-challenge
+
+Link to actual challenge: https://www.freecodecamp.com/challenges/build-a-tribute-page
---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+  $('.ul-trivia').children().addClass('trivia-text');
+  $('.trivia-text').css("padding-top", '10px');
+  $('.ul-trivia').css("padding-left", '200px');
+});
